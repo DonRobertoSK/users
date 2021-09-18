56f22a0e6c6eb32149d17b356c84588170b45f58
Robert Kristofic <robo.kristofic@gmail.com>
2021-09-18 14:48:54 +0200

    Rename table USERS to SUERS

--- src/main/java/sk/skog/users/data/User.java
+++ src/main/java/sk/skog/users/data/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "SUSERS")
 public class User implements Serializable {
 	@Id
 	@Column(name = "USER_ID")
